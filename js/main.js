b6ad6d1c92a9e4dad2d1cd718dde80f2171839f8
Nahuel <staudtnahuel24@gmail.com>
2023-12-20 12:08:34 -0300

    carrito funcionando+libreria

--- js/main.js
+++ js/main.js
@@ -1,218 +1,126 @@
-// PRODUCTOS
-const productos = [
-    // NIKE
-    {
-        id: "zapatilla-01",
-        titulo: "Air Jordan 1 Retro High OG",
-        imagen: "./img/nike/Air Jordan 1 Retro High OG.webp",
-        categoria: {
-            nombre: "Nike",
-            id: "nike"
-        },
-        precio: 10000
-    },
-    {
+let productos = [];
 
-        id: "zapatilla-02",
-        titulo: "Air Jordan 1 Zoom Comfort 2",
-        imagen: "./img/nike/Air Jordan 1 Zoom Comfort 2.webp",
-        categoria: {
-            nombre: "Nike",
-            id: "nike"
-        },
-        precio: 10000
-    },
-    {
-        id: "zapatilla-03",
-        titulo: "Air Jordan XXXVII Tatum",
-        imagen: "./img/nike/Air Jordan XXXVII Tatum.webp",
-        categoria: {
-            nombre: "Nike",
-            id: "nike"
-        },
-        precio: 10000
-    },
-    {
-        id: "zapatilla-04",
-        titulo: "Jumpman MVP",
-        imagen: "./img/nike/Jumpman MVP.webp",
-        categoria: {
-            nombre: "Nike",
-            id: "ike"
-        },
-        precio: 10000
-    },
-    {
-        id: "zapatilla-05",
-        titulo: "Nike Air Force 1 Low Retro",
-        imagen: "./img/nike/Nike Air Force 1 Low Retro.webp",
-        categoria: {
-            nombre: "Nike",
-            id: "nike"
-        },
-        precio: 10000
-    },
-    {
-        id: "zapatilla-06",
-        titulo: "Nike Air Force 1 Mid 07 LV8",
-        imagen: "./img/nike/Nike Air Force 1 Mid 07 LV8.webp",
-        categoria: {
-            nombre: "Nike",
-            id: "nike"
-        },
-        precio: 10000
-    },
-    {
-        id: "zapatilla-07",
-        titulo: "Nike Air Max 97 OG",
-        imagen: "./img/nike/Nike Dunk Mid.webp",
-        categoria: {
-            nombre: "Nike",
-            id: "nike"
-        },
-        precio: 10000
-    },
+fetch("./js/productos.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        cargarProductos(productos);
+    })
 
-    {
-        id: "zapatilla-08",
-        titulo: "Nike Kiger 9",
-        imagen: "./img/nike/Nike Kiger 9.webp",
-        categoria: {
-            nombre: "Nike",
-            id: "nike"
-        },
-        precio: 10000
-    },
-    //ADIDAS
-    {
-        id: "zapatilla-9",
-        titulo: "ZAPATILLAS ADIDAS 4D KRAZED",
-        imagen: "./img/adidas/ZAPATILLAS ADIDAS 4D KRAZED.avif",
-        categoria: {
-            nombre: "Adidas",
-            id: "adidas"
-        },
-        precio: 10000
-    },
-    {
-        id: "zapatilla-10",
-        titulo: "ADIDAS DURAMO SL 2.0",
-        imagen: "./img/adidas/ZAPATILLAS DURAMO SL 2.0.avif",
-        categoria: {
-            nombre: "Adidas",
-            id: "adidas"
-        },
-        precio: 10000
-    },
-    {
-        id: "zapatilla-11",
-        titulo: "ADIDAS FORUM BOLD",
-        imagen: "./img/adidas/ZAPATILLAS FORUM BOLD.avif",
-        categoria: {
-            nombre: "Adidas",
-            id: "didas"
-        },
-        precio: 10000
-    },
-    {
-        id: "zapatilla-12",
-        titulo: "ADIDAS FORUM MID",
-        imagen: "./img/adidas/ZAPATILLAS FORUM MID.avif",
-        categoria: {
-            nombre: "Adidas",
-            id: "didas"
-        },
-        precio: 10000
-    },
-    {
-        id: "zapatilla-13",
-        titulo: "ADIDAS NMD_R1",
-        imagen: "./img/adidas/ZAPATILLAS NMD_R1.avif",
-        categoria: {
-            nombre: "Adidas",
-            id: "Adidas"
-        },
-        precio: 10000
-    },
-    {
-        id: "zapatilla-14",
-        titulo: "ADIDAS STAN SMITH X ANDRÉ SARAIVA",
-        imagen: "./img/adidas/ZAPATILLAS STAN SMITH X ANDRÉ SARAIVA.avif",
-        categoria: {
-            nombre: "Adidas",
-            id: "adidas"
-        },
-        precio: 10000
-    },
-    {
-        id: "zapatilla-15",
-        titulo: "ADIDAS SUPERSTAR",
-        imagen: "./img/adidas/ZAPATILLAS SUPERSTAR.avif",
-        categoria: {
-            nombre: "Adidas",
-            id: "adidas"
-        },
-        precio: 10000
-    },
-    {
-        id: "zapatilla-16",
-        titulo: "ADIDAS TOP TEN RB",
-        imagen: "./img/adidas/ZAPATILLAS TOP TEN RB.avif",
-        categoria: {
-            nombre: "Adidas",
-            id: "adidas"
-        },
-        precio: 10000
-    },
-    {
-        id: "zapatilla-17",
-        titulo: "Nike Kiger 9",
-        imagen: "./img/adidas/Zapatillas_Adifom_Q_Azul_HQ4324_01_standard.avif",
-        categoria: {
-            nombre: "Adidas",
-            id: "adidas"
-        },
-        precio: 10000
-    },
-];
 
-const contenedorProductos = document.querySelector("#contenedorProductos")
-const botonesCategorias = document.querySelectorAll(".botonCategoria")
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
+
+botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+}))
+
 
 function cargarProductos(productosElegidos) {
 
-    contenedorProductos.innerHTML="";
+    contenedorProductos.innerHTML = "";
 
     productosElegidos.forEach(producto => {
+
         const div = document.createElement("div");
         div.classList.add("producto");
         div.innerHTML = `
-            <img class="pruductoImagen" src="${producto.imagen}" alt="${producto.titulo}">
-            <div class="productoDetalles">
-                <h3 class="productoTitulo">${producto.titulo}</h3>
-                <p class="productoTexto">$${producto.precio}</p>
-                <button class="productoAgregar" id="${producto.id}">Agregar</button>
-            </div>`;
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `;
+
         contenedorProductos.append(div);
     })
+
+    actualizarBotonesAgregar();
 }
 
-cargarProductos(productos);
 
 botonesCategorias.forEach(boton => {
-
     boton.addEventListener("click", (e) => {
 
         botonesCategorias.forEach(boton => boton.classList.remove("active"));
         e.currentTarget.classList.add("active");
 
-        const categoriaSeleccionada = e.currentTarget.id;
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
 
-        const productosBoton = productos.filter(producto => producto.categoria.id === categoriaSeleccionada);
-
-        if(categoriaSeleccionada === "todos") return cargarProductos(productos);
-        
-        return cargarProductos(productosBoton);
     })
-    
 });
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+        background: "linear-gradient(to right, #4b33a8, #785ce9)",
+        borderRadius: "2rem",
+        textTransform: "uppercase",
+        fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem',
+            y: '1.5rem'
+        },
+        onClick: function(){}
+    }).showToast();
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
